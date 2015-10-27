4f14de280bf5b445b1c58f8f0d492f77dcadbf5e
Geoffrey Bell <gbell@bericotechnologies.com>
2015-10-26 23:26:38 -0400

    code clean up

--- parking-sensor.ino
+++ parking-sensor.ino
@@ -6,11 +6,6 @@ const int SONAR_PIN = A0;
 
 // The final distance value
 double distance = 0.0;
-int MIN = 112;
-// Used to store the intensity level of the individual LEDs, intitialized to full brightness (0)
-int redIntensity = 0;
-int greenIntensity = 0;
-int blueIntensity = 0;
 
 void setup() {
     pinMode(SONAR_PIN, INPUT);
@@ -18,19 +13,14 @@ void setup() {
     pinMode(GREEN_LED_PIN, OUTPUT);
     pinMode(BLUE_LED_PIN, OUTPUT);
     // Register a Spark variable here that we can get via curl
-    Spark.variable("dist", &distance, DOUBLE);
+    Spark.variable("distance", &distance, DOUBLE);
 }
 
 void loop() {
     int reading = 0;
-    double voltage = 0.0;
-
     reading = analogRead(SONAR_PIN);
-    voltage = reading + MIN;
-
     // 9.8v per inch
     distance = reading / 9.8;
-    Serial.println(distance); //for debugging
 
     if (distance < 24) {
         // Red
